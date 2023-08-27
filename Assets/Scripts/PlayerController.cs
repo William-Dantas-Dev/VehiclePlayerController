8b86ada7e1356cca0f656b44157b9db3c3c3c091
William-Dantas-Dev <95831281+William-Dantas-Dev@users.noreply.github.com>
2023-08-27 15:07:57 -0300

    car controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
